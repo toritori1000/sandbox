98b82aa1d0e81eff5ffe2c09247c882219931853
toritori1000 <toritori1000@gmail.com>
2018-06-19 22:12:22 -0400

    polls end of tutorial pt 3

---
+++ polls/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+from .models import Question
+
+
+# Create your views here.
+
+def index(request):
+    q_list = Question.objects.order_by('-pub_date')
+
+    context = {
+        'q_list': q_list,
+    }
+
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/detail.html', context)
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
